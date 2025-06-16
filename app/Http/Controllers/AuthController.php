2b91e9b39f791d6db63ab425d9a48fd5beb6938b
jongmin <thswhdals@naver.com>
2025-06-16 12:39:11 +0900

    중간 체크

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function loginForm(Request $request){
+        $data = [
+            'pageTitle'=>'Login',
+        ];
+        return view('back.pages.auth.login', $data);
+    }
+
+    public function forgotForm(Request $request){
+        $data = [
+            'pageTitle'=>'forgot',
+        ];
+
+        return view('back.pages.auth.forgot', $data);
+    }
+}
